dfef69a7c7c00dc201dbb917c87e88a2b3e69100
ssjang1 <s104183@naver.com>
2024-05-23 20:47:26 +0900

    first commit

---
+++ PIDNet/person_on_road.py
@@ -0,0 +1,51 @@
+import cv2
+import os
+import torch
+import torch.nn.functional as F
+import numpy as np
+from loguru import logger
+
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+color_map = [(128, 64,128),
+             (244, 35,232),
+             ( 70, 70, 70),
+             (102,102,156),
+             (190,153,153),
+             (153,153,153),
+             (250,170, 30),
+             (220,220,  0),
+             (107,142, 35),
+             (152,251,152),
+             ( 70,130,180),
+             (220, 20, 60),
+             (255,  0,  0),
+             (  0,  0,142),
+             (  0,  0, 70),
+             (  0, 60,100),
+             (  0, 80,100),
+             (  0,  0,230),
+             (119, 11, 32)]
+
+def input_transform(image):
+    image = image.astype(np.float32)[:, :, ::-1]
+    image = image / 255.0
+    image -= mean
+    image /= std
+    return image
+
+def load_pretrained(model, pretrained):
+    pretrained_dict = torch.load(pretrained, map_location='cpu')
+    if 'state_dict' in pretrained_dict:
+        pretrained_dict = pretrained_dict['state_dict']
+    model_dict = model.state_dict()
+    pretrained_dict = {k[6:]: v for k, v in pretrained_dict.items() if (k[6:] in model_dict and v.shape == model_dict[k[6:]].shape)}
+    msg = 'Loaded {} parameters!'.format(len(pretrained_dict))
+    logger.info('load_weights_start')
+    logger.info(msg)
+    logger.info('load_weights_done')
+    model_dict.update(pretrained_dict)
+    model.load_state_dict(model_dict, strict = False)
+    
+    return model
+    
